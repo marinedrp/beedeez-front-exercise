e3c090e3feb7fce7ad290b42c0f01e27c38cd7fe
marinedrp <marine.drp@outlook.com>
2023-03-28 22:07:45 +0200

    tried to implement the infinite scroll on the home page

--- front/src/screens/Home.tsx
+++ front/src/screens/Home.tsx
@@ -1,33 +1,40 @@
 import {useEffect, useState} from 'react';
 import {FlatList, StyleSheet, View, ActivityIndicator} from 'react-native';
-import { Loader } from '../components/Loader';
+import {Loader} from '../components/Loader';
 import {StationItem} from '../components/StationItem';
 import {Station} from '../interfaces/Station';
 import api from '../services/api';
 
+const PAGE_SIZE = 20;
+
 export const Home = () => {
   const [stations, setStations] = useState<Station[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchStations();
-  }, []);
+  }, [currentPage]);
 
   const fetchStations = async () => {
     setIsLoading(true);
+
     try {
+      const endIndex = currentPage * PAGE_SIZE;
       const response = await api.get('/stations');
-      setStations(response.data.data);
+      setStations(prevStations => [...prevStations, ...response.data.data.slice(0, endIndex)]);
       setIsLoading(false);
     } catch (error: any) {
       console.error(error);
     }
   };
 
+  const fetchNextPage = () => {
+    setCurrentPage(prevCurrentPage => prevCurrentPage + 1);
+  };
+
   const renderLoader = () => {
-    return isLoading ? (
-      <Loader />
-    ) : null;
+    return isLoading ? <Loader /> : null;
   };
 
   const renderItem = ({item}: {item: Station}) => <StationItem item={item} />;
@@ -36,9 +43,10 @@ export const Home = () => {
     <FlatList
       data={stations}
       renderItem={renderItem}
-      keyExtractor={item => item._id.toString()}
+      keyExtractor={item => item._id}
+      onEndReached={fetchNextPage}
+      onEndReachedThreshold={0}
       ListFooterComponent={renderLoader}
     />
   );
 };
-
