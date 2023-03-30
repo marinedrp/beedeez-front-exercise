c215a6d2fe48d4daa3820287d19dd5862f5ce88e
marinedrp <marine.drp@outlook.com>
2023-03-30 17:59:52 +0200

    handle when the users reaches the end of the list

--- back/src/services/stations.service.ts
+++ back/src/services/stations.service.ts
@@ -1,5 +1,22 @@
 import stationsModel, { Station } from '@models/stations.model';
 
+// ideally: implementing pagination as provided by the MongoDB documentation:
+/*
+function printStudents(startValue, nPerPage) {
+  let endValue = null;
+  db.students.find( { _id: { $lt: startValue } } )
+             .sort( { _id: -1 } )
+             .limit( nPerPage )
+             .forEach( student => {
+               print( student.name );
+               endValue = student._id;
+             } );
+
+  return endValue;
+
+  
+*/
+
 class StationsService {
   public async findAllStations(): Promise<Station[]> {
     const stations: Station[] = await stationsModel.find();
--- front/src/screens/Home/Home.tsx
+++ front/src/screens/Home/Home.tsx
@@ -1,42 +1,53 @@
-import React, { useEffect } from 'react';
-import { FlatList, Text, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store/store';
-import { Loader } from '../../components/Loader/Loader';
-import { StationItem } from '../../components/StationItem/StationItem';
-import { Station } from '../../types/station';
-import { LogoutButton } from '../../components/LogoutButton/LogoutButton';
-import { fetchStations, selectStations, selectLoading, selectError } from '../../slices/stationsSlice';
-
+import {useEffect, useState} from 'react';
+import {FlatList, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '../../store/store';
+import {Loader} from '../../components/Loader/Loader';
+import {StationItem} from '../../components/StationItem/StationItem';
+import {Station} from '../../types/station';
+import {LogoutButton} from '../../components/LogoutButton/LogoutButton';
+import {
+  fetchStations,
+  selectStations,
+  selectLoading,
+  selectError,
+} from '../../slices/stationsSlice';
 
 export const Home = () => {
-  const REFRESH_INTERVAL = 60000; 
-  const dispatch: AppDispatch = useDispatch()
+  const REFRESH_INTERVAL = 120000;
+  const dispatch: AppDispatch = useDispatch();
   const email = useSelector((state: RootState) => state.auth.user?.email);
   const stations = useSelector(selectStations);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
+  const [itemsToShow, setItemsToShow] = useState(10);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
 
   useEffect(() => {
     dispatch(fetchStations());
-    
+
     const intervalId = setInterval(() => {
       dispatch(fetchStations());
     }, REFRESH_INTERVAL);
-    console.log(stations)
 
     return () => clearInterval(intervalId);
   }, [dispatch]);
 
-  const renderItem = ({ item }: { item: Station }) => <StationItem item={item} />;
+  const renderItem = ({item}: {item: Station}) => <StationItem item={item} />;
 
   const renderLoader = () => {
     return loading ? <Loader /> : null;
   };
 
   const loadMoreStations = () => {
-    if (!loading) {
-      dispatch(fetchStations());
+    if (!loading && hasMoreItems) {
+      setItemsToShow(prevItemsToShow => {
+        const newItemsToShow = prevItemsToShow + 10;
+        if (newItemsToShow >= stations.length) {
+          setHasMoreItems(false);
+        }
+        return newItemsToShow;
+      });
     }
   };
 
@@ -44,16 +55,15 @@ export const Home = () => {
     <>
       <LogoutButton />
       <Text>Welcome {email}!</Text>
-      {error ? (<Text>{error}</Text>) : (
-        <FlatList
-        data={stations}
+      <FlatList
+        data={stations.slice(0, itemsToShow)}
         renderItem={renderItem}
         keyExtractor={item => item._id.toString()}
         onEndReached={loadMoreStations}
         onEndReachedThreshold={0}
         ListFooterComponent={renderLoader}
+        ListEmptyComponent={<Text>No stations available.</Text>}
       />
-      )}
     </>
   );
 };
